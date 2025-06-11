ac2b81b4afbbbd4ffaf954baeaa5fa4fcb4508ca
Edilmer Elí Vasquez Narva <7196803631@untrm.edu.pe>
2025-06-11 12:54:20 -0500

    Comit 3

--- VASQUEZ_NARVA_EDILMER_ELI_EWEB/index.php
+++ VASQUEZ_NARVA_EDILMER_ELI_EWEB/index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro Tesis</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Registro</h1>
     
     <p><a href="GTesis.php">Gestion Tesis</a></p>
     <p><a href="GTesista.php">Gestion Tesista</a></p>
-    <p><a href="https://github.com/edilmer2107/EXAMEN_WEB.git">Gestion Tesista</a></p>
+    <p><a href="https://github.com/edilmer2107/EXAMEN_WEB.git">Git Hub</a></p>
     
 </body>
 </html>
